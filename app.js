f44427b5a44d82206367f2ae1f106e3929549c78
Hana Maruškevičová <hanka.maruskevicova@gmail.com>
2023-12-31 23:51:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function flipAllCards(time) {
 }
 
 
-const countToDate = new Date("2024-03-31");
+const countToDate = new Date("2024-01-01");
 let previousTimeBetweenDates;
 setInterval(() => {
     const currentDate = new Date();
